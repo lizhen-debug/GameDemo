171f374b7b0aa3b078e005af4af9dbc3b38c673c
lizhen-debug <1012082742@qq.com>
2022-05-28 17:12:57 +0800

    lee

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Event : MonoBehaviour
 {
     public Player player0;
-    
+    public GameObject random_event_canvas;
     // Start is called before the first frame update
     void Start()
     {
- 
+        random_event_canvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,5 +21,10 @@ public class Event : MonoBehaviour
     public void IncreasePlayerFoodValue()
     {
         player0.ChangePlayerInfo(Player.info_type.FOOD,Player.operate_type.INCREASE,10);
+    }
+
+    public void ShowRandomEvent()
+    {
+        random_event_canvas.SetActive(true);
     }
 }
