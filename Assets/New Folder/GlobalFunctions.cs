fa43e49a62b820269659d99cb82af6924d16e65d
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2022-05-28 01:26:41 -0500

    camera switch script done

--- Assets/New Folder/GlobalFunctions.cs
+++ Assets/New Folder/GlobalFunctions.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalFunctions : MonoBehaviour
+public static class GlobalFunctions
 {
-    // Start is called before the first frame update
-    void Start()
+    public static List<GameObject> cam_arr = new List<GameObject>();
+    public static int cur_cam_idx = 0;
+    public static void ChangeCamera(int cam_idx)
     {
-        
+        cam_arr[cam_idx].SetActive(true);
+        foreach(GameObject cam in cam_arr)
+        {
+            if (cam == cam_arr[cam_idx]) { continue; }
+            cam.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
