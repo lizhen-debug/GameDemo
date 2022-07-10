09de39c7e692a86add5f4edd6f4c060ac628b606
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2022-07-10 04:28:06 -0500

    knife move

--- Assets/Scripts/knife_move.cs
+++ Assets/Scripts/knife_move.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class knife_move : MonoBehaviour
 {
-    
+    float timer = 0;
+    int hit_frame_defined = 10;
+    int hit_frame;
+    float val;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hit_frame = hit_frame_defined;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (hit_frame > 0 && val > -0.1 && val < 0.1)
+        {
+            Debug.Log("if: " + hit_frame);
+            hit_frame--;
+        }
+        else
+        {
+            Debug.Log("else: " + hit_frame);
+            timer += Time.fixedDeltaTime;
+            val = Mathf.Sin(timer);
+            hit_frame = hit_frame_defined;
+            gameObject.transform.position += new Vector3(0, Mathf.Sin(timer), 0);
+        }
+       
         if (Input.GetKeyDown("space"))
         {
             CutVegetable();
@@ -22,6 +40,6 @@ public class knife_move : MonoBehaviour
 
     void CutVegetable()
     {
-        gameObject.transform.position += new Vector3(0, 1, 0);
+        gameObject.transform.position += new Vector3(-1, 0, 0);
     }
 }
